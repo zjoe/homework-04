b99c93e95ea3058d7e18e4afd41af48989e3776b
Forwil <yu10022@qq.com>
2013-10-28 12:02:12 +0800

    finish

--- test.c
+++ test.c
@@ -21,14 +21,16 @@ struct Node queue[MAXTREESIZE];
 struct Go pos[8] = {
 {0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1}
 };
-/* note for *pos*
+/* 
+ * note for *pos*
  * 0 is right
  * 1 is right down
  * 2 is down
  * 3 is ... you know
  * it is deasil(顺时针)
+ *
  */
-char words[MAXNUM][MAXLEN],matrix[MAXMAT][MAXMAT];
+char words[MAXNUM][MAXLEN],matrix[MAXMAT][MAXMAT],out[MAXMAT*2][MAXMAT*2];
 int numw,n,m,numnode;
 
 int ctoint(char c)
@@ -107,6 +109,58 @@ int testallword()
 	return 0;
 }
 
+void addtag(int x,int y,int p,int k)
+{
+	int i,g;
+	char c;
+	for(i = 0;i < strlen(words[k])-2;i++)
+		{
+		if (p == 0 || p == 4) c = '-';
+		if (p == 1 || p == 5) c = '\\';
+		if (p == 2 || p == 6) c = '|';
+		if (p == 3 || p == 7) c = '/';
+		out[2*x + pos[p].x][2*y + pos[p].y] = c;
+		x += pos[p].x;
+		y += pos[p].y;
+		}
+	return ;
+}
+
+void outans()
+{
+	int i,j,p,k;
+	for(i=0;i<n;i++)
+		for(j=0;j<m;j++)
+			{
+			out[i*2][j*2] = matrix[i][j];
+			out[i*2+1][j*2] = ' ' ;
+			out[i*2][j*2+1] = ' ' ;
+			out[i*2+1][j*2+1] = ' ' ;
+			}
+	for(i=0;i<n;i++)
+		for(j=0;j<m;j++)
+			for(p=0;p<8;p++)
+			{
+				k = searchnode(i,j,p);
+				if(k!=-1)
+					addtag(i,j,p,k);
+			}
+	for(i=0;i<n*2-1;i++)
+		printf("%s\n",out[i]);
+}
+
+int testcorner()
+{
+	if(out[0][1]==' ' && out[1][0]==' ' && out[1][1]==' ')
+		return -1;
+	if(out[2*n-1][0]==' ' && out[2*n][1]==' ' && out[2*n-1][1]==' ')
+		return -1;
+	if(out[0][2*m-1]==' ' && out[1][2*m]==' ' && out[1][2*m-1]==' ')
+		return -1;
+	if(out[2*n-1][2*m]==' ' && out[2*n][2*m-1]==' ' && out[2*n-1][2*m-1]==' ')
+		return -1;
+	return 0;
+}
 
 int main(void)
 {
@@ -125,13 +179,20 @@ int main(void)
 	m = strlen(matrix[0])-1;
 
 	if(testallword()==0)
+		{
 		printf("STATE1: is good!\n");
+		outans();
+		}
 	else
 		printf("STATE1: bad!\n");
 	if(n==m)
-		printf("STATE2: is good\n");
+		printf("STATE2: is good! n&m = %d\n",n);
 	else 
-		printf("STATE2: bad!\n");
+		printf("STATE2: bad! n=%d m=%d\n",n,m);
+	if(testcorner() == 0)
+		printf("STATE3: is good!\n");
+	else
+		printf("STATE3: bad!\n");
 	return 0;
 }
 
