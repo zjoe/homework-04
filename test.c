d505da9a8a71c6642c10f3b7852088b13bce9422
Forwil <yu10022@qq.com>
2013-10-28 11:28:36 +0800

    finish beta 1

--- test.c
+++ test.c
@@ -3,28 +3,32 @@
 #define MAXTREESIZE 2000
 #define MAXLEN 22
 #define MAXNUM 63
+#define MAXMAT 100
+
 struct Node
 {
 	int flag;
 	int p[26];
 };
+
 struct Go
 {
 	int x;
 	int y;
 };
+
 struct Node queue[MAXTREESIZE];
 struct Go pos[8] = {
-{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,-1}
+{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1}
 };
-/*
+/* note for *pos*
  * 0 is right
  * 1 is right down
  * 2 is down
  * 3 is ... you know
  * it is deasil(顺时针)
  */
-char words[MAXNUM][MAXLEN],matrix[100][100];
+char words[MAXNUM][MAXLEN],matrix[MAXMAT][MAXMAT];
 int numw,n,m,numnode;
 
 int ctoint(char c)
@@ -68,7 +72,6 @@ int searchnode(int x,int y, int p)
 	int id = 0,cid;
 	for(;x>=0&&y>=0&&x<n&&y<m; x+=pos[p].x,y+=pos[p].y)
 		{
-	//	printf("%x\n",ctoint(matrix[x][y]));
 		cid = queue[id].p[ctoint(matrix[x][y])];
 		if (cid == -1)
 			return -1;
@@ -76,7 +79,34 @@ int searchnode(int x,int y, int p)
 		if (queue[id].flag != -1)
 			return queue[id].flag;
 		}
+	return -1;
 }
+
+int testallword()
+{
+	int i,j,p,count = 0,k,boo[MAXNUM]={0};
+	for(i=0;i<n;i++)
+		for(j=0;j<m;j++)	
+			for(p=0;p<8;p++)
+			{
+				k = searchnode(i,j,p);
+				if (k != -1)
+					{
+					//printf("%d %d %c %d %s",i,j,matrix[i][j],p,words[k]);
+					if (boo[k] == 0)
+						{
+						boo[k] = 1;
+						count +=1;
+						}
+					else
+						return -1;
+					}
+			}
+	if (count != numw)
+		return -1;
+	return 0;
+}
+
 
 int main(void)
 {
@@ -89,13 +119,20 @@ int main(void)
 		fgets(words[i],MAXLEN,stdin);
 		if ((j = insertnode(i,words[i]))==-1)
 			return -1;
-		//printf("%d %d\n",i,j);
 		}
 	while(fgets(matrix[n],100,stdin)>0)
-		n += 1;
+		n += 1; 
 	m = strlen(matrix[0])-1;
-	//printf("%d %d",numnode,numw);
-	//printf("%d\n%d\n",searchnode(0,0,0),searchnode(2,0,1));
-	//for(i=0;i<26;i++)printf("%d ",queue[0].p[i]);
+
+	if(testallword()==0)
+		printf("STATE1: is good!\n");
+	else
+		printf("STATE1: bad!\n");
+	if(n==m)
+		printf("STATE2: is good\n");
+	else 
+		printf("STATE2: bad!\n");
+	return 0;
 }
+
 
